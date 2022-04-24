5df1ede1938e0c2c1c0150ef6340812aa349f641
0x11111111 <dypro_mike@outlook.com>
2022-04-24 12:02:19 +0800

    Bug repair: Scoop issue.

--- src/core_neomask_finger_tracking.py
+++ src/core_neomask_finger_tracking.py
@@ -665,69 +665,69 @@ def fingerprint_erase(group_number):
     cv2.destroyAllWindows()
 
 
-if __name__ == '__main__':
-    debug_mode = SimpleNamespace(
-        circle_on=False,
-        landmark_on=False,
-        coordination_on=False,
-        output_on=False,
-        orientation_on=False,
-        frame_rate_on=True,
-        scoop_on=False,
-    )
+debug_mode = SimpleNamespace(
+    circle_on=False,
+    landmark_on=False,
+    coordination_on=False,
+    output_on=False,
+    orientation_on=False,
+    frame_rate_on=True,
+    scoop_on=False,
+)
 
-    args = SimpleNamespace(
-        folder=os.path.join('../.tmp', '{}'.format(int(round(time.time() * 1000))))
-    )
+args = SimpleNamespace(
+    folder=os.path.join('../.tmp', '{}'.format(int(round(time.time() * 1000))))
+)
 
-    mp_drawing = mediapipe.solutions.drawing_utils
-    mp_hands = mediapipe.solutions.hands
+mp_drawing = mediapipe.solutions.drawing_utils
+mp_hands = mediapipe.solutions.hands
 
-    if not os.path.exists("../.tmp"):
-        os.mkdir("../.tmp")
+if not os.path.exists("../.tmp"):
+    os.mkdir("../.tmp")
 
-    os.mkdir(args.folder)
+os.mkdir(args.folder)
 
-    EPS = 0.0001
-    fingertip_radius_sn = SimpleNamespace(
-        thumb=20.0,
-        index=18.4,
-        middle=18.3,
-        ring=17.3,
-        pinky=15.3
-    )
-    finger_length_sn = SimpleNamespace(
-        thumb=32.1,
-        index=24.7,
-        middle=26.4,
-        ring=26.3,
-        pinky=23.7
-    )
-    tip_dip_length_ratio = 0.8
-    pinky_ring_width_ratio = 0.89
-    thumb_width_length_ratio = 0.60
-    finger_mcp_width_ratio = 0.65
+EPS = 0.0001
+fingertip_radius_sn = SimpleNamespace(
+    thumb=20.0,
+    index=18.4,
+    middle=18.3,
+    ring=17.3,
+    pinky=15.3
+)
+finger_length_sn = SimpleNamespace(
+    thumb=32.1,
+    index=24.7,
+    middle=26.4,
+    ring=26.3,
+    pinky=23.7
+)
+tip_dip_length_ratio = 0.8
+pinky_ring_width_ratio = 0.89
+thumb_width_length_ratio = 0.60
+finger_mcp_width_ratio = 0.65
 
-    landmark_order = 'abcdefghijklmnopqrstu'
-    image_list = []
+landmark_order = 'abcdefghijklmnopqrstu'
+image_list = []
 
-    selection = get_option()
-    args.file_path = selection['file_path']
-    # Kernel size of Gaussian should be odd only
-    args.kernel_size = int(selection['blur_value'] // 2 * 2 + 1)
+selection = get_option()
+args.file_path = selection['file_path']
+# Kernel size of Gaussian should be odd only
+args.kernel_size = int(selection['blur_value'] // 2 * 2 + 1)
 
-    args.blur_mode = 0
-    if selection['normalization']:
-        args.blur_mode = 1
-    elif selection['gaussian']:
-        args.blur_mode = 2
+args.blur_mode = 0
+if selection['normalization']:
+    args.blur_mode = 1
+elif selection['gaussian']:
+    args.blur_mode = 2
 
-    cap = cv2.VideoCapture(selection['file_path'])
-    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    cap.release()
-    num_processes = mp.cpu_count()
-    frame_jump_unit = frame_count // num_processes
+cap = cv2.VideoCapture(selection['file_path'])
+frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+cap.release()
+num_processes = mp.cpu_count()
+frame_jump_unit = frame_count // num_processes
 
-    p = mp.Pool(num_processes)
-    p.map(fingerprint_erase, range(num_processes))
+p = mp.Pool(num_processes)
+p.map(fingerprint_erase, range(num_processes))
+
 
