8a143f233bfb70d04636dea7590a73326449dce9
0x11111111 <dypro_mike@outlook.com>
2022-04-24 13:16:01 +0800

    Test version.

--- src/core_neomask_finger_tracking.py
+++ src/core_neomask_finger_tracking.py
@@ -1,14 +1,11 @@
-import mediapipe
 import cv2
 import os
 import time
 import math
 import numpy as np
-import multiprocessing as mp
-from functools import partial
 from types import SimpleNamespace
-from gui_get_option import get_option
 
+from main import *
 
 def calculate_distance_sn(coord_sn_1, coord_sn_2):
     return math.sqrt((coord_sn_1.x - coord_sn_2.x) ** 2 + (coord_sn_1.y - coord_sn_2.y) ** 2)
@@ -582,12 +579,12 @@ def fingerprint_erase(_args, group_number):
         os.mkdir(temp_path)
     # video_source = 0
     cap = cv2.VideoCapture(video_source)
-    cap.set(cv2.CAP_PROP_POS_FRAMES, args.frame_jump_unit * group_number)
+    cap.set(cv2.CAP_PROP_POS_FRAMES, _args.frame_jump_unit * group_number)
     proc_frames = 0
 
     try:
         with mp_hands.Hands(min_detection_confidence=0.6, min_tracking_confidence=0.6) as hands:
-            while proc_frames < args.frame_jump_unit:
+            while proc_frames < _args.frame_jump_unit:
                 success, frame = cap.read()
 
                 if not success:
@@ -663,73 +660,4 @@ def fingerprint_erase(_args, group_number):
 
     cap.release()
     cv2.destroyAllWindows()
-
-
-if __name__ == '__main__':
-    mp.freeze_support()
-
-    debug_mode = SimpleNamespace(
-        circle_on=False,
-        landmark_on=False,
-        coordination_on=False,
-        output_on=False,
-        orientation_on=False,
-        frame_rate_on=True,
-        scoop_on=False,
-    )
-
-    args = SimpleNamespace(
-        folder=os.path.join('../.tmp', '{}'.format(int(round(time.time() * 1000))))
-    )
-
-    mp_drawing = mediapipe.solutions.drawing_utils
-    mp_hands = mediapipe.solutions.hands
-
-    if not os.path.exists("../.tmp"):
-        os.mkdir("../.tmp")
-
-    os.mkdir(args.folder)
-
-    EPS = 0.0001
-    fingertip_radius_sn = SimpleNamespace(
-        thumb=20.0,
-        index=18.4,
-        middle=18.3,
-        ring=17.3,
-        pinky=15.3
-    )
-    finger_length_sn = SimpleNamespace(
-        thumb=32.1,
-        index=24.7,
-        middle=26.4,
-        ring=26.3,
-        pinky=23.7
-    )
-    tip_dip_length_ratio = 0.8
-    pinky_ring_width_ratio = 0.89
-    thumb_width_length_ratio = 0.60
-    finger_mcp_width_ratio = 0.65
-
-    landmark_order = 'abcdefghijklmnopqrstu'
-    image_list = []
-
-    selection = get_option()
-    args.file_path = selection['file_path']
-    # Kernel size of Gaussian should be odd only
-    args.kernel_size = int(selection['blur_value'] // 2 * 2 + 1)
-
-    args.blur_mode = 0
-    if selection['normalization']:
-        args.blur_mode = 1
-    elif selection['gaussian']:
-        args.blur_mode = 2
-
-    cap = cv2.VideoCapture(selection['file_path'])
-    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    cap.release()
-    num_processes = mp.cpu_count()
-    args.frame_jump_unit = frame_count // num_processes
-
-    p = mp.Pool(num_processes)
-    p.map(partial(fingerprint_erase, args), range(num_processes))
 
